fbd8fbbc504c179d46b0222c60de3fc9cbeddb7e
Protato_Plus <robo6178@gmail.com>
2024-03-17 21:11:10 -0700

    Initial config stuff, dont use it yet

---
+++ carbon/config.cpp
@@ -0,0 +1,13 @@
+class config {
+	bool initConfig(const char[]) {
+
+	}
+	template<typename T>
+	T ReadData(const char[]) {
+
+	}
+	template<typename T>
+	T WriteData(const char[]) {
+
+	}
+};
--- carbon/config.h
+++ carbon/config.h
@@ -1,5 +1,26 @@
 #pragma once
 #include <stdint.h>
 // this is a really big fucking hack, im gonna rewrite this later
+// edit: That time is now!
+
+
+// =========  WHAT YOU NEED TO KNOW ABOUT CONFIG  =========
+//
+// the config class should work for whatever you need
+// after creating the object make sure to call 
+// initConfig with the name of the config you would like
+// to open. if the given config doesnt exist one will
+// be created with that name. Read and write your objects
+// with ReadData and WriteData, they both are templates
+// so specify your own types (and please dont do dumb things with this).
+
+class config {
+	bool initConfig(const char[]);
+	template<typename T>
+	T ReadData(const char[]);
+	template<typename T>
+	T WriteData(const char[]);
+};
+
 static const unsigned int confWidth = 1680;
 static const unsigned int confHeight = 1050;
