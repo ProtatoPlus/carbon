b1a8fe69ea497a64e7f801613d2b1a28e56f1a2b
Protato_Plus <robo6178@gmail.com>
2024-03-23 19:26:51 -0700

    Fix config

--- carbon/config.cpp
+++ carbon/config.cpp
@@ -1,13 +1,11 @@
 #include "config.h"
-class config {
-	CSimpleIniA iniFile;
-	SI_Error rc;
-	bool initConfig(const char* confName) {
-		iniFile.SetUnicode();
-		rc = iniFile.LoadFile(confName);
-		if (rc < 0) {
-			logError(HIGH, "Failed to initialize config file.");
-			return false;
-		};
-	}
-};
+CSimpleIniA iniFile;
+SI_Error rc;
+bool initConfig(const char* confName) {
+	iniFile.SetUnicode();
+	rc = iniFile.LoadFile(confName);
+	if (rc < 0) {
+		logError(HIGH, "Failed to initialize config file.");
+		return false;
+	};
+}
