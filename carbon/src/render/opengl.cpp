75f265b1ea3a7b2b26295034a44e87a4d948ce6d
Protato_Plus <robo6178@gmail.com>
2024-04-09 20:47:56 -0700

    First triangle!

--- carbon/src/render/opengl.cpp
+++ carbon/src/render/opengl.cpp
@@ -2,14 +2,113 @@
 #include <glad/glad.h>
 
 bool initGL() {
+	logMessage(INFO, "OpenGL init");
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		logError(CRITICAL, "Failed to initialize GLAD");
 		return false;
 	}
 	glViewport(0, 0, 800, 600);
+
+	compileShaders();
+
+	if (!createShaderProgram()) {
+		return false;
+	}
+
+	debugGeometry();
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	return true;
+}
+
+bool compileShaders() {
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		logError(HIGH, "ERROR::SHADER::VERTEX::COMPILATION_FAILED");
+		return false;
+	}
+	logMessage(INFO, "Compiled vertex shader");
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		logError(HIGH, "ERROR::SHADER::VERTEX::COMPILATION_FAILED");
+		return false;
+	}
+	logMessage(INFO, "Compiled fragment shader");
+}
+
+bool createShaderProgram() {
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	int  success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		logError(HIGH, "Failed to create shader program");
+		return false;
+	}
+	logMessage(INFO, "Created shader program");
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	return true;
+}
+
+void debugGeometry() {
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 }
 
 void doRender() {
 	glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- carbon/src/render/opengl.h
+++ carbon/src/render/opengl.h
@@ -2,5 +2,14 @@
 #include "../util/logging.h"
 #include "../engine/window.h"
 #include "../../include/glfw/glfw3.h"
+static unsigned int vertexShader;
+static unsigned int fragmentShader;
+static unsigned int shaderProgram;
+static unsigned int VBO;
+static unsigned int VAO;
 bool initGL();
+bool compileShaders();
+bool createShaderProgram();
+void debugGeometry();
+void drawTriangle();
 void doRender();
